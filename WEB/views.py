1aee29808e2714da4095ccee33e8e9f37a0db460
kostikan <ea56@mail.ru>
2020-03-24 16:56:11 +0300

    update Paginator

--- WEB/views.py
+++ WEB/views.py
@@ -4,28 +4,29 @@ from django.urls import path
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 questions = []
-for i in range(1,30):
-    questions.append({'id' : i,'title': f'question # {i}'})
-
+for i in range(1, 30):
+    questions.append({'id': i, 'title': f'question # {i}'})
 
 
 def paginate(objects_list, request):
     paginator = Paginator(objects_list, 4)
     page = request.GET.get('page')
     try:
-         list = paginator.get_page(page)
+        page_number = paginator.get_page(page)
 
     except PageNotAnInteger:
-        list = paginator.page(1)
+        page_number = paginator.page(1)
 
     except EmptyPage:
-        list = paginator.page(paginator.num_pages)
-    return list
+        page_number = paginator.page(paginator.num_pages)
+    return page_number
+
 
 def MainPage(request):
     pagData = paginate(questions, request)
     rendered_data = {"questions": pagData}
     return render(request, 'MainPage.html', rendered_data)
+
 
 def hot(request):
     return render(request, 'MainPage.html', {
